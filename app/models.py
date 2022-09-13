d1b55560bac9d46feba43f02e9605f0b694c0028
Makim1998 <mmijatovic32@gmail.com>
2022-09-13 16:13:41 +0200

    feat(login): add profile and followings to model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Numeric, Integer, String, Date
+from sqlalchemy import Table, Boolean, Column, ForeignKey, Numeric, Integer, String, Date
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -21,3 +21,27 @@ class User(Base):
     # posts = relationship("Post", back_populates="author")
     # reactions = relationship("Reaction", back_populates="author")
 
+
+association_table = Table(
+    "association",
+    Base.metadata,
+    Column("follower_id", Integer, ForeignKey("profiles.id"), primary_key=True),
+    Column("following_id", Integer, ForeignKey("profiles.id"), primary_key=True),
+)
+
+
+class Profile(Base):
+    __tablename__ = "profiles"
+    id = Column(Integer(), primary_key=True)
+    picture = Column(String(80))
+    description = Column(String())
+    private = Column(Boolean())
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="profile")
+    following = relationship("Profile",
+                             secondary=association_table,
+                             primaryjoin=id==association_table.c.follower_id,
+                             secondaryjoin=id==association_table.c.following_id,
+                             backref="follower")
+    
+    # following koga on prati, follower ko njega prati
