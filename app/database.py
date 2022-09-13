2a4f2909414c406376cfe3d6e57eb70334f3fcfd
Makim1998 <mmijatovic32@gmail.com>
2022-09-13 17:47:58 +0200

    fix: relative path of import changed to absolute path

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-import models
+import app.models as models
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
 engine = create_engine(
